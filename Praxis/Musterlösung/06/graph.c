d3dd0e8b19db7288196e35fa61865a04b87d5917
Johannes Hayeß <jhaye@gmx.de>
2018-12-05 15:32:26 +0100

    Free alloc. memory and deref pointers, insted of '->'
    Also add scope delimiters for for loops and use proper pointer
    types for malloc size.

--- Praxis/Musterlösung/06/graph.c
+++ Praxis/Musterlösung/06/graph.c
@@ -25,7 +25,7 @@ int main (void) {
     vertex v_3_3 = { .x = 3, .y = 3 };
     vertex v_3_1 = { .x = 3, .y = 1 };
 
-    vertex **v_set = malloc(sizeof(void*) * 4);
+    vertex **v_set = malloc(sizeof(vertex*) * 4);
 
     v_set[0] = &v_1_1;
     v_set[1] = &v_1_3;
@@ -34,20 +34,20 @@ int main (void) {
 
     edge e_1_1_1_3 = { .from = &v_1_1, .to = &v_1_3, 1 };
     edge e_1_3_1_1 = { .from = &v_1_3, .to = &v_1_1, 1 };
-    
+
     edge e_1_1_3_3 = { .from = &v_1_1, .to = &v_3_3, 1 };
     edge e_3_3_1_1 = { .from = &v_3_3, .to = &v_1_1, 1 };
-    
+
     edge e_1_1_3_1 = { .from = &v_1_1, .to = &v_3_1, 1 };
     edge e_3_1_1_1 = { .from = &v_3_1, .to = &v_1_1, 1 };
-    
+
     edge e_1_3_3_3 = { .from = &v_1_3, .to = &v_3_3, 1 };
     edge e_3_3_1_3 = { .from = &v_3_3, .to = &v_1_3, 1 };
-    
+
     edge e_3_1_3_3 = { .from = &v_3_1, .to = &v_3_3, 1 };
     edge e_3_3_3_1 = { .from = &v_3_3, .to = &v_3_1, 1 };
 
-    edge **e_set = malloc(sizeof(void*) * 10);
+    edge **e_set = malloc(sizeof(edge*) * 10);
 
     e_set[0] = &e_1_1_1_3;
     e_set[1] = &e_1_3_1_1;
@@ -62,11 +62,19 @@ int main (void) {
 
     graph g = { .v = v_set, .e = e_set, .v_count = 4, .e_count = 10 };
 
-    for (size_t i = 0; i < g.v_count; ++i)
-        printf("Vertex %zu: (%li,%li)\n", i, g.v[i]->x, g.v[i]->y);
+    for (size_t i = 0; i < g.v_count; ++i) {
+        printf("Vertex %zu: (%li,%li)\n", i, (*(g.v[i])).x, (*(g.v[i])).y);
+    }
+
     printf("\n");
-    for (size_t i = 0; i < g.e_count; ++i)
-        printf("Edge %zu: (%li,%li) -> (%li,%li)\n", i, g.e[i]->from->x, g.e[i]->from->y, g.e[i]->to->x, g.e[i]->to->y);
+
+    for (size_t i = 0; i < g.e_count; ++i) {
+        printf("Edge %zu: (%li,%li) -> (%li,%li)\n", i, (*(g.e[i])).from->x, (*(g.e[i])).from->y, (*((*(g.e[i])).to)).x, (*((*(g.e[i])).to)).y);
+    }
+
+    // always free memory previously allocated
+    free(v_set);
+    free(e_set);
 
     return EXIT_SUCCESS;
 }

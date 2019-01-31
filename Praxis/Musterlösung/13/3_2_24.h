8996d4fd9dad35a4affd4e6f9ad3aaa440ad87f8
cnikkor <mkskoebeln@web.de>
2019-01-31 13:36:20 +0100

    add 3.2.24 result remove 3.2.8 partial result

---
+++ Praxis/Musterlösung/13/3_2_24.h
@@ -0,0 +1,23 @@
+int prim(int x) {
+  for (size_t i; i < x; ++i) {
+    if (x % i == 0) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+typedef struct elem *l_Ptr;
+typedef struct elem {
+  int key;
+  l_Ptr next;
+  l_elem;
+}
+
+void prim_ex(l_Ptr *l) {
+  l_Ptr next = l->next;
+  if(prim(next->key)) {
+    free(l->next);
+    l->next =  next->next;
+  }
+}
